442bd2214aaac5e2644f89ffc910c184cc43fb56
Naimur2 <naimurrahaman248@gmail.com>
2021-02-06 19:01:27 +0600

    fixed

--- index.js
+++ index.js
@@ -10,45 +10,64 @@ const weatherImgs={
  lightsnow:"lightsnow.png",
  thunderstorminvicinity:"thunderstorm.png",
  haze:"fog.png",
-
+ snow:"snow.png"
 
 };
 
 let CurrentTemperature= location =>{
-    fetch(`http://api.weatherstack.com/current?access_key=ea378c552d112c70720216c31793cf34&query=${location}`)
-    .then(response => response.json())
-    .then(data => {
-        let condition=(data.current.weather_descriptions)[0];
-        let name=condition.replace(/\s+/g, '').toLowerCase();
-        let imgLink;
-        if (weatherImgs.hasOwnProperty(name)){
-            imgLink=`images/${weatherImgs[name]}`;
-        }
-       else{
-           imgLink=(data.current.weather_icons)[0];
-       }
-        let weather=`
-        <h2 class="weather-place">${data.request.query}</h2>
-        <div style="background-image:url('${imgLink}')" class="weather-image"></div>
-        <h2 class="weather-situation">${condition}</h2>
-        <h1 class="weather-temperature">${data.current.temperature}° c</h1>
-      `
-      weatherDesc.innerHTML=weather;
-      console.log(data.current.weather_icons);
+    fetch (`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f6ac34ed76a847a5fab2dbce2ce86ea5`)
+            .then(response => response.json())
+            .then(data => {
+                let condition=data.weather[0].main;
+                    let name=condition.replace(/\s+/g, '').toLowerCase();
+                    let imgLink;
+                    if (weatherImgs.hasOwnProperty(name)){
+                        imgLink=`images/${weatherImgs[name]}`;
+                    }
+                else{
+                    imgLink='images/sunny.png';
+                }
+                let weather=`
+                    <h2 class="weather-place">${data.name}</h2>
+                <div style="background-image:url('${imgLink}')" class="weather-image"></div>
+                <h2 class="weather-situation">${condition}</h2>
+                <h1 class="weather-temperature">${Math.floor(data.main.temp-274)}° c</h1>`
+                weatherDesc.innerHTML=weather;
+                
+            console.log(data);
+            })
+            .catch(err =>alert("Something went wrong"));
+    // fetch(`http://api.weatherstack.com/current?access_key=ea378c552d112c70720216c31793cf34&query=${location}`)
+    // .then(response => response.json())
+    // .then(data => {
+    //     let condition=(data.current.weather_descriptions)[0];
+    //     console.log(condition);
+    //     let name=condition.replace(/\s+/g, '').toLowerCase();
+    //     let imgLink;
+    //     if (weatherImgs.hasOwnProperty(name)){
+    //         imgLink=`images/${weatherImgs[name]}`;
+    //     }
+    //    else{
+    //        imgLink=images/sunny.png;
+    //    }
+    //     let weather=`
+    //     <h2 class="weather-place">${data.request.query}</h2>
+    //     <div style="background-image:url('${imgLink}')" class="weather-image"></div>
+    //     <h2 class="weather-situation">${condition}</h2>
+    //     <h1 class="weather-temperature">${data.current.temperature}° c</h1>
+    //   `
+    //   weatherDesc.innerHTML=weather;
+     
         
-    })
-    .catch(err=> console.log(err));
+    // })
+    // .catch(err=> console.log(err));
     
 };
 
-let city="Dhaka";
-CurrentTemperature(city);
-fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f6ac34ed76a847a5fab2dbce2ce86ea5`)
-.then(response => response.json())
-.then(data => {test.innerHTML=data.weather[0].main;
-console.log(data.weather);
-})
-.catch(err =>alert(err));
+
+
+    CurrentTemperature("Dhaka");
+
 
 
 button.addEventListener("click",()=>{
